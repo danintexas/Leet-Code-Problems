66436d3ac30c114cb87c6115c836cd1ad2cc0d80
Daniel Gail <daniel.gail@gmail.com>
2020-02-26 15:37:34 -0600

    Cleaned up LC1

--- Leet Code Problems/LC1/Two_Sum.cs
+++ Leet Code Problems/LC1/Two_Sum.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿/* https://leetcode.com/problems/two-sum/
+ * 
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+*/
+
+using System;
 
 namespace Leet_Code_Problems
 {
-    class Two_Sum_1
+    class Two_Sum
     {
         public static void LC1()
         {
             // Variables Needed
             int[] twoSumNums = { 1, 2, 5, 6, 7 };
             int twoSumTarget = 6;
-            int[] response = Two_Sum_1.TwoSum(twoSumNums, twoSumTarget);
+            int[] response = TwoSum(twoSumNums, twoSumTarget);
             string sentArray = "";
             string responseArray = "[ " + response[0].ToString() + "," + response[1].ToString() + " ]";
 
