15c1a2061dc271fbd89137dd78258701755506ff
Daniel Gail <daniel.gail@gmail.com>
2020-02-29 11:26:58 -0600

    Created LC14 template

---
+++ Leet Code Problems/Easy/LC14/Longest_Common_Prefix.cs
@@ -0,0 +1,35 @@
+﻿/* https://leetcode.com/problems/longest-common-prefix/
+ * 
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * 
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Example 1:
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ * Example 2:
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * 
+ * Note: All given inputs are in lowercase letters a-z.
+ */
+
+using System;
+
+namespace Leet_Code_Problems.Easy.LC14
+{
+    class Longest_Common_Prefix
+    {
+        public static void LC14()
+        {
+            
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            return "yes";
+        }
+    }
+}
